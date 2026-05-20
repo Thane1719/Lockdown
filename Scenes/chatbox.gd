6cb6fbf134905e1049326b9a21af037393fef46a
James Reed <0852520@schoolsnet.act.edu.au>
2026-05-20 11:14:06 +1000

    Finshed!
    kinda. its a little buggy. WASD still works when your in the chatbox but thats it. Multiplayer works on my end

--- Scenes/chatbox.gd
+++ Scenes/chatbox.gd
@@ -1,34 +1,43 @@
 extends Node2D
 
-@onready var Message = $Message          
-@onready var Send = $Send                
-@onready var Messages = $Messages        
-@onready var Chatbox = self       
+@onready var Message = $Message
+@onready var Send = $Send
+@onready var Messages = $Messages
+@onready var Chatbox = self
 
-
-func _ready():
-	Chatbox.hide() #Hides the chatbox
+func _ready(): #Hides the chatbox
+	Chatbox.hide()
 	Send.pressed.connect(_on_send_pressed)
-
 
 func _process(delta):
 	if Input.is_action_just_pressed("chatbox"):
-		Chatbox.visible = !Chatbox.visible   # Toggle visibility
-		Input.MOUSE_MODE_CONFINED_HIDDEN         #Shows the mouse.
+		Chatbox.visible = !Chatbox.visible
 
+		if Chatbox.visible: #Changes the mouse mode and auto focus's the chatbox.
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			Message.grab_focus()
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			Message.release_focus()
 
 func _on_send_pressed() -> void:
+
 	var text = Message.text.strip_edges()
 
-	if text == "":
-		return  # Don't send empty messages
+	if text == "": # Don't send empty messages
+		return
 
-	# Creates a new label for the message
-	var new_message = Label.new()
-	new_message.text = text
+	# Sends the message to everybody
+	send_message.rpc(multiplayer.get_unique_id(), text)
 
-	# Add it to the message box
-	Messages.add_child(new_message)
-
-	
 	Message.text = ""
+
+# Multiplayer Fuction
+@rpc("any_peer", "call_local")
+func send_message(player_id, text):
+	
+# Creates a new label for the message
+	var new_message = Label.new()
+	new_message.text = str(player_id) + ": " + text
+
+	Messages.add_child(new_message)
